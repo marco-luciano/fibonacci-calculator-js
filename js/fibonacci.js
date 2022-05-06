10042c1d66cc33f2b63c98139e291c7d9a55afe5
Marco Magisano <marco.magisano.7@gmail.com>
2022-05-07 02:19:19 +0300

    Milestone 3.1 - Geekout

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -34,23 +34,16 @@ function fibonacci() {
 function fibonacciCalc(n) {
     if (!n) {
         return 0;
-    } else if (n === 1) {
+    }
+
+    if (n === 1) {
         return 1;
-    } else if (n > 0 && n <= FIBONACCI_MAX_N_VALUE) {
-        let result = 0;
+    }
 
-        // set i and j counters as 0 and 1, initial values of fibonacci series
-        let i = 0;
-        let j = 1;
-
-        for (let k = 1; k < n; k++) {
-            result = i + j;
-            i = j;
-            j = result;
-        }
-
-        return result;
+    if (n > 1 && n <= FIBONACCI_MAX_N_VALUE) {
+        return fibonacciCalc(n - 1) + fibonacciCalc(n - 2);
     } else {
+        // out of bounds
         return -1;
     }
 }
