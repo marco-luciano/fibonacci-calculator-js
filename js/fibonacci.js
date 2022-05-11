68f95cd2281866eaf5ec965630d2ddee4754f19a
Marco Magisano <69358294+marco-luciano@users.noreply.github.com>
2022-05-11 03:55:26 +0300

    Milestone 4

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,4 +1,5 @@
 const FIBONACCI_MAX_N_VALUE = 50;
+const FIBONACCI_MAIN_URL = "http://localhost:5050";
 
 document
     .getElementById("fibonacciNumberInput")
@@ -17,21 +18,71 @@ document
         fibonacci();
     });
 
-function fibonacci() {
+async function fibonacci() {
     let n = parseInt(document.getElementById("fibonacciNumberInput").value);
-    let result = fibonacciCalc(n);
+    let result = await fibonacciCalc(n);
 
     let resultInput = document.getElementById("result");
 
-    if (result === -1) {
-        // if out of bounds...
+    if (typeof result === "undefined") {
+        // if something went wrong (network error, bad request/out of bounds)...
         result = ""; // blank
     }
 
     resultInput.innerHTML = result; // result replaced
 }
 
-function fibonacciCalc(n) {
+async function fibonacciCalc(n) {
+    if (n) {
+        var result = await fetch(FIBONACCI_MAIN_URL + "/fibonacci/" + n)
+            .then(async function (response) {
+                if (!response.ok) {
+                    // bad request, server error
+                    throw Error(await response.text());
+                }
+
+                return response.json();
+            })
+            .then((data) => {
+                return data.result;
+            })
+            // network error
+            .catch((error) => console.error(error));
+    } else {
+        // temporary behavior until next Milestone
+        console.error("Error: Not a number");
+    }
+
+    return result;
+}
+
+// Replaced by server request
+function fibonacciCalcIterative(n) {
+    if (!n) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else if (n > 0 && n <= FIBONACCI_MAX_N_VALUE) {
+        let result = 0;
+
+        // set i and j counters as 0 and 1, initial values of fibonacci series
+        let i = 0;
+        let j = 1;
+
+        for (let k = 1; k < n; k++) {
+            result = i + j;
+            i = j;
+            j = result;
+        }
+
+        return result;
+    } else {
+        return -1;
+    }
+}
+
+// Replaced by server request
+function fibonacciCalcRecursive(n) {
     if (!n) {
         return 0;
     }
