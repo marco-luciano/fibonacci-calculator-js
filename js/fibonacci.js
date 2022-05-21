58d7c6128ae1d2624b42e946b42433ae35642d44
Marco Magisano <69358294+marco-luciano@users.noreply.github.com>
2022-05-22 02:30:57 +0300

    Milestone 6

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,6 +1,10 @@
 const FIBONACCI_MAX_N_VALUE = 50;
 const FIBONACCI_MAIN_URL = "http://localhost:5050";
 
+window.addEventListener("load", (event) => {
+    loadFibonacciResults(drawFibonacciResults);
+});
+
 document
     .getElementById("fibonacciNumberInput")
     .addEventListener("keypress", function (event) {
@@ -16,100 +20,80 @@ document
     .getElementById("fibonacciButton")
     .addEventListener("click", function (event) {
         hideAlerts();
-        showSpinner();
-
+        showElement("fibonacciSpinner");
         fibonacci();
     });
 
-async function fibonacci() {
+function fibonacci() {
     let n = parseInt(document.getElementById("fibonacciNumberInput").value);
-
-    let result = await fibonacciCalc(n);
-
-    if (typeof result === "undefined") {
-        // if something went wrong (network error, bad request/out of bounds)...
-        result = ""; // blank
-    }
-
-    hideSpinner();
-    showResult(result);
+    let calc = fibonacciCalc(n);
 }
 
-async function fibonacciCalc(n) {
+function fibonacciCalc(n) {
     if (n >= 0 && n <= 50) {
-        var result = await fetch(FIBONACCI_MAIN_URL + "/fibonacci/" + n)
-            .then(async function (response) {
+        let result = fetch(FIBONACCI_MAIN_URL + "/fibonacci/" + n)
+            .then(function (response) {
                 if (!response.ok) {
-                    throw Error(await response.text());
+                    return response.text();
                 }
                 return response.json();
             })
             .then((data) => {
-                return data.result;
+                hideElement("fibonacciSpinner");
+
+                if (data.number) {
+                    showResult(data.result);
+                    loadFibonacciResults(drawFibonacciLastResult);
+                } else {
+                    displayServerError("Server " + data);
+                }
             })
-            .catch(async function (error) {
+            .catch(function (error) {
                 displayServerError("Server " + error);
             });
-    } else if (n >= 50) {
-        await displayValidationError("Can’t be larger than 50");
-    } else {
-        await displayValidationError("Error: Not a valid number");
-    }
-
-    return result;
-}
-
-// Replaced by server request
-function fibonacciCalcIterative(n) {
-    if (!n) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
-    } else if (n > 0 && n <= FIBONACCI_MAX_N_VALUE) {
-        let result = 0;
-
-        // set i and j counters as 0 and 1, initial values of fibonacci series
-        let i = 0;
-        let j = 1;
-
-        for (let k = 1; k < n; k++) {
-            result = i + j;
-            i = j;
-            j = result;
-        }
 
         return result;
+    } else if (n >= 50) {
+        displayValidationError("Can’t be larger than 50");
     } else {
-        return -1;
+        displayValidationError("Error: Not a valid number");
     }
 }
 
-// Replaced by server request
-function fibonacciCalcRecursive(n) {
-    if (!n) {
-        return 0;
-    }
+function loadFibonacciResults(callback) {
+    showElement("fibonacciResultsSpinner");
+    let results = fetch(FIBONACCI_MAIN_URL + "/getFibonacciResults")
+        .then(function (response) {
+            if (!response.ok) {
+                throw Error(response.text());
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            fibonacciRecords = data;
+            // sort results descending by timestamp
+            fibonacciRecords.results.sort(
+                (x, y) => y.createdDate - x.createdDate
+            );
+            callback(fibonacciRecords);
+            hideElement("fibonacciResultsSpinner");
+            return fibonacciRecords;
+        })
+        .catch(function (error) {
+            displayServerError("Server " + error);
+        });
 
-    if (n === 1) {
-        return 1;
-    }
-
-    if (n > 1 && n <= FIBONACCI_MAX_N_VALUE) {
-        return fibonacciCalc(n - 1) + fibonacciCalc(n - 2);
-    } else {
-        // out of bounds
-        return -1;
-    }
+    return results;
 }
 
-function hideSpinner() {
-    let spinner = document.getElementById("fibonacciSpinner");
-    spinner.style.setProperty("display", "none");
+function hideElement(id) {
+    let element = document.getElementById(id);
+    element.style.setProperty("display", "none");
 }
 
-function showSpinner() {
-    let spinner = document.getElementById("fibonacciSpinner");
-    spinner.style.setProperty("display", "block");
+function showElement(id) {
+    let element = document.getElementById(id);
+    element.style.setProperty("display", "block");
 }
 
 function hideResult() {
@@ -130,10 +114,11 @@ function hideValidationError() {
     validation.style.setProperty("display", "none");
 }
 
-async function displayValidationError(message) {
+function displayValidationError(message) {
     let validation = document.getElementById("alertValidation");
     validation.innerHTML = message;
     validation.style.display = "block";
+    hideElement("fibonacciSpinner");
 }
 
 function hideServerError() {
@@ -145,11 +130,37 @@ function displayServerError(message) {
     let alertServer = document.getElementById("alertServer");
     alertServer.innerHTML = message;
     alertServer.style.display = "block";
+    hideElement("fibonacciSpinner");
 }
 
-function hideAlerts(){
+function hideAlerts() {
     hideResult();
     hideValidationError();
     hideServerError();
 }
 
+function drawFibonacciResults(results) {
+    results.results.forEach(function (element) {
+        const record = document.createElement("tr");
+        record.className = "fibonacci-record";
+        record.innerHTML = formatFibonacciRecord(element);
+        document
+            .querySelector(".fibonacci-results .fibonacci-results-table")
+            .append(record);
+    });
+}
+
+function drawFibonacciLastResult(results) {
+    const record = document.createElement("tr");
+    record.className = "fibonacci-record";
+    record.innerHTML = formatFibonacciRecord(results.results[0]);
+    document
+        .querySelector(".fibonacci-results .fibonacci-results-table")
+        .prepend(record);
+}
+
+function formatFibonacciRecord(record) {
+    let date = new Date(record["createdDate"]);
+    text = `The Fibonnaci Of <b>${record["number"]}</b> is <b>${record.result}</b>. Calculated at: ${date} `;
+    return text;
+}
